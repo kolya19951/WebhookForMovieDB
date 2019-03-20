0b7189995be003da7549f55744d97eb3ebc29583
Mykola Symotiuk <symotiuk@edsson.com>
2019-03-20 14:12:55 +0200

    json repare

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ server.post('/get-movie-details', (req, res) => {
             dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
             return res.json({
-                message: {
-                speech: dataToSend,
+                fulfillmentMessages: {
+                text: dataToSend,
                 displayText: dataToSend,
                 source: 'get-movie-details'
                 }
